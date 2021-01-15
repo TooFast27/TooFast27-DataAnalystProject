78de3bafebf6242175cb98d51c21827b6e39ac5b
Giuseppe <giuseppecarnimeo96@gmail.com>
2021-01-15 22:51:33 +0100

    CreatedDWH witrh Log tables

---
+++ DWH/DB.WebAgencyDWH/Control/Tables/ExecutionLog.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [Control].[ExecutionLog] (
+    [ExecutionLogID] INT           IDENTITY (1, 1) NOT NULL,
+    [PackageName]    VARCHAR (200) NOT NULL,
+    [StartDate]      DATETIME      NOT NULL,
+    [EndDate]        DATETIME      NULL,
+    [StatusID]       INT           NOT NULL,
+    CONSTRAINT [PK_ExecutionLog] PRIMARY KEY CLUSTERED ([ExecutionLogID] ASC),
+    CONSTRAINT [FK_ExecutionLog_ExecutionLogStatusID] FOREIGN KEY ([StatusID]) REFERENCES [Control].[ExecutionLogStatus] ([StatusID])
+);
+
---
+++ DWH/DB.WebAgencyDWH/Control/Tables/ExecutionLogStatus.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [Control].[ExecutionLogStatus] (
+    [StatusID]    INT          IDENTITY (1, 1) NOT NULL,
+    [Description] VARCHAR (20) NOT NULL,
+    CONSTRAINT [PK_ExecutionLogStatus] PRIMARY KEY CLUSTERED ([StatusID] ASC)
+);
+
