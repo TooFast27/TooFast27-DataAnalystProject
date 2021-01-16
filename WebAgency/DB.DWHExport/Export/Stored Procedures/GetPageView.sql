894c10edd5109a44ce1c82a5367b7a05f6b1e6d8
Giuseppe <giuseppecarnimeo96@gmail.com>
2021-01-16 01:45:07 +0100

    Export Procedure fro page table

---
+++ WebAgency/DB.DWHExport/Export/Stored Procedures/GetPageView.sql
@@ -0,0 +1,22 @@
+﻿-- =============================================
+-- Author:	Giuseppe Carnimeo
+-- Create date: 20210115
+-- Description: This procedures select the incremental records from the PageViewTable into the OLTP db
+-- Parameters: @StartTime
+-- =============================================
+CREATE PROCEDURE [Export].[GetPageView]
+	@StartTime datetime = '19900101'
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT 
+		[PageViewID],
+		[UserID],
+		[PageviewDatetime],
+		[Url]
+	FROM [WebAgency].[dbo].[PageView]
+	WHERE [PageviewDatetime]> @StartTime
+
+
+END
--- WebAgency/DB.DWHExport/Security/Export.sql
+++ WebAgency/DB.DWHExport/Security/Export.sql
@@ -1,3 +1,5 @@
 ﻿CREATE SCHEMA [Export]
     AUTHORIZATION [dbo];
 
+
+
